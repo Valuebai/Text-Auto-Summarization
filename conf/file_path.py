d221fdcd7d6b5900d54509e553c5bf98fcbbeace
luhuibo <luhuibo306@foxmail.com>
2019-10-14 17:56:33 +0800

    【新增】参数配置，方便后面使用log，databases, file_path等

---
+++ conf/file_path.py
@@ -0,0 +1,36 @@
+# 该文件主要设置file path，统一在这里进行修改
+# 使用方法：在其他文件中导入
+
+import platform
+from pathlib import Path
+
+# 获取系统的信息
+sysstr = platform.system()
+if (sysstr == "Windows"):
+    print("Call Windows tasks")
+    # 需要设置windows的配置路径
+    sys_path = Path(r'C:/AI-NLP/Text-Auto-Summarization')
+    # ltp模型目录的路径
+    LTP_DATA_DIR = Path(r'C:/AI-NLP/Text-Auto-Summarization/data/ltp_data_v3.4.0')
+elif (sysstr == "Linux"):
+    print("Call Linux tasks")
+    # 需要设置linux的配置路径
+    sys_path = Path(r'/home/student/project/project-01/kill_bug_team/Text-Auto-Summarization')
+    # ltp模型目录的路径
+    LTP_DATA_DIR = Path(r'/home/student/project/project-01/kill_bug_team/Text-Auto-Summarization/data/ltp_data_v3.4.0')
+else:
+    print("Other System tasks")
+
+# 读取/保存文本的路径
+
+
+# 分词模型路径，模型名称为`cws.model`
+cws_model_path = LTP_DATA_DIR / r'cws.model'
+# 词性标注模型路径，模型名称为`pos.model`
+pos_model_path = LTP_DATA_DIR / r'pos.model'
+# 命名实体识别模型路径，模型名称为`pos.model`
+ner_model_path = LTP_DATA_DIR / r'ner.model'
+# 依存句法分析模型路径，模型名称为`parser.model`
+par_model_path = LTP_DATA_DIR / r'parser.model'
+# 语义角色标注模型目录路径，模型目录为`srl`
+srl_model_path = LTP_DATA_DIR / r'pisrl.model'
